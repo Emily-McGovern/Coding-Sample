ce23fa94345c83be8c44941273fc85183d691251
Emily.McGovern <emily.mcgovern@unsw.edu.au>
2022-06-26 13:51:05 +1000

    update gitignore

--- manipulation.R
+++ manipulation.R
@@ -96,14 +96,15 @@ p <-
   theme_minimal()+
   theme(
     plot.margin = margin(1.5, 2.5, 0.5, 1.75, "cm"),
-    plot.title = element_text(hjust = 0.5, size = 25, face = 'bold'), 
-    plot.subtitle = element_text(hjust = 0.5, size = 17),
-    axis.text = element_text(size = 10, color = 'black', face = 'bold'), 
-    axis.text.x=element_text(angle=45,hjust=1),
-    strip.text = element_text(size = 14, face = 'bold'), 
+    plot.title = element_text(hjust = 0.5, size = 30, face = 'bold'), 
+    plot.subtitle = element_text(hjust = 0.5, size = 20),
+    axis.text = element_text(size = 17, color = 'black', face = 'bold'), 
+    axis.text.x=element_text(angle=45,hjust=1, size = 17),
+    axis.title=element_text(size=17,face="bold"),
+    strip.text = element_text(size = 17, face = 'bold'), 
     panel.grid = element_line(color = 'gray'), 
     legend.position = 'bottom',
-    legend.key.width=unit(0.8,"cm"),
+    legend.key.width=unit(1,"cm"),
     legend.key.height=unit(0.2,"cm"),
     axis.ticks = element_line(colour = "grey70", size = 0.2),
     panel.grid.major = element_line(colour = "grey70", size = 0.2),
@@ -112,7 +113,7 @@ p <-
 
 # Save plot
 ggsave(filename = (paste0(here("out/"), out.dir, "/mpplot-", out.dir, ".png")), 
-       width = 40, height = 20, dpi = 320, units = "cm")
+       width = 40, height = 30, dpi = 320, units = "cm")
 
 
 
