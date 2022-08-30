a4fe9c27c3a26cb2c9c921838239602556a9c77f
Emily-McGovern <54656921+Emily-McGovern@users.noreply.github.com>
2022-08-30 15:15:22 +0100

    Update get_last_weekdate.R

--- R/get_last_weekdate.R
+++ R/get_last_weekdate.R
@@ -3,7 +3,7 @@ get_last_weekdate <- function() {
   if(lubridate::wday(Sys.Date()) ==1){
     date<-Sys.Date()-2
   }
-  else if(lubridate::wday(Sys.Date()) ==6){
+  else if(lubridate::wday(Sys.Date()) ==7){
     date <- Sys.Date()-1
   } 
   else{
